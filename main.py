413690ba69bba7b38912079bbc85d0a2c5e008ca
Ivan <58259789+Preston-138@users.noreply.github.com>
2022-04-26 06:48:53 +0000

    Добавлена функция subdomains_fuzzing()

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 import requests 
+import time
+
 
 def update_db():
   """создаем текстовый файл-словарь с гитхаба"""
-  r = requests.get('https://raw.githubusercontent.com/daviddias/node-dirbuster/master/lists/directory-list-2.3-medium.txt')
+  r = requests.get('https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/DNS/namelist.txt')
   wordlist = r.text
-  with open('dirbuster_wordlist.txt', 'w') as f:
+  with open('namelist.txt', 'w') as f:
     print(wordlist, file=f)
 
-#update_db()
+# update_db()
 
 def start_fuzzing():
+  """Фаззинг веб-контента"""
   target = input('Введи url: ') # http://pascher.world/
   with open('dirbuster_wordlist.txt', 'r') as f:
     for fuzz in f:
       target_url = target + fuzz
+      print(target_url)
       r = requests.get(target_url)
       if r.status_code == 200:
         print(target_url, r.status_code)
@@ -21,4 +25,20 @@ def start_fuzzing():
         print('interesting ', target_url, r.status_code)
   print('OK')
 
-start_fuzzing()
+
+def subdomains_fuzzing():
+  """Фаззинг поддоменов"""
+  with open('namelist.txt', 'r') as f:
+    for fuzz in f:
+      target_url = f'http://{fuzz}.pascher.world'
+      r = requests.get(target_url)
+      if r.status_code == 200:
+        print(target_url, r.status_code)
+      if r.status_code != 200 and r.status_code != 404:
+        print('interesting ', target_url, r.status_code)
+        time.sleep(0.3)
+      
+  print('OK')
+
+# start_fuzzing()
+subdomains_fuzzing()
